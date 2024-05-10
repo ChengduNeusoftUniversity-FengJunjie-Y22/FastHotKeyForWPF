3706ab774fb209315dd72650b81470dec54ccaab
冯 <2789083329@qq.com>
2024-05-10 22:29:06 +0800

    增量更新（测试，Nuget不更新）

---
+++ ComponentInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+namespace FastHotKeyForWPF
+{
+    public class ComponentInfo
+    {
+        public ComponentInfo() { }
+
+        public ComponentInfo(double fontsize, SolidColorBrush foreground, SolidColorBrush background, Thickness margin)
+        {
+            FontSize = fontsize;
+            Foreground = foreground;
+            Background = background;
+            Margin = margin;
+        }
+
+        private double _fontsize = 20;
+
+        private double _marginup = 0;
+        private double _marginright = 0;
+        private double _margindown = 0;
+        private double _marginleft = 0;
+
+        public double FontSize
+        {
+            get { return _fontsize; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _fontsize = value;
+                }
+            }
+        }
+
+        public SolidColorBrush Foreground = Brushes.Black;
+
+        public SolidColorBrush Background = Brushes.White;
+
+        public Thickness Margin
+        {
+            get { return new Thickness(_marginup, _marginright, _margindown, _marginleft); }
+            set
+            {
+                if (value.Left < 0 || value.Right < 0 || value.Top < 0 || value.Bottom < 0)
+                {
+                    return;
+                }
+                _marginup = value.Left;
+                _marginright = value.Top;
+                _margindown = value.Right;
+                _marginleft = value.Bottom;
+            }
+        }
+    }
+}
--- PrefabComponent.cs
+++ PrefabComponent.cs
@@ -4,29 +4,45 @@ namespace FastHotKeyForWPF
 {
     public class PrefabComponent
     {
+        public static ComponentInfo? TempInfo;
+
         public static T GetComponent<T>() where T : class, new()
         {
             return new T();
         }
 
-        public static T GetComponent<T>(SizeInfo info) where T : class, new()
+        public static T GetComponent<T>(ComponentInfo info) where T : class, new()
         {
-            return new T();
+            TempInfo = info;
+
+            T result = new T();
+
+            TempInfo = null;
+            return result;
         }
 
         public class KeySelectBox : TextBox
         {
             public KeySelectBox()
             {
-
+                if (TempInfo == null) { return; }
+                FontSize = TempInfo.FontSize;
+                Foreground = TempInfo.Foreground;
+                Background = TempInfo.Background;
+                Margin = TempInfo.Margin;
             }
+
         }
 
         public class HotKeyNameBlock : TextBlock
         {
             public HotKeyNameBlock()
             {
-
+                if (TempInfo == null) { return; }
+                FontSize = TempInfo.FontSize;
+                Foreground = TempInfo.Foreground;
+                Background = TempInfo.Background;
+                Margin = TempInfo.Margin;
             }
         }
     }
