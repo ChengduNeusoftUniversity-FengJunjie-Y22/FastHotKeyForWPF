fb0515fa9bc11e1df1441a9b135651583085e8c3
冯 <2789083329@qq.com>
2024-05-10 10:26:42 +0800

    添加一个新的类，用于热键设置界面的快速搭建

---
+++ PrefabComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Controls;
+
+namespace FastHotKeyForWPF
+{
+    public class PrefabComponent
+    {
+        public static T GetComponent<T>() where T : class, new()
+        {
+            T component = new T();
+            return component;
+        }
+
+        public class KeySelectBox : TextBox
+        {
+            public KeySelectBox()
+            {
+
+            }
+        }
+
+        public class HotKeyNameBlock : TextBlock
+        {
+            public HotKeyNameBlock()
+            {
+
+            }
+        }
+    }
+}
