e9f5aab747d770ce231bb23892471041fea60856
冯 <2789083329@qq.com>
2024-05-11 14:25:07 +0800

    增量更新

--- KeySelectBox.cs
+++ KeySelectBox.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows;
+using System.Reflection;
+using System.Windows.Media;
+
+public enum KeyTypes
+{
+    Normal,
+    Model,
+    None
+}
 
 namespace FastHotKeyForWPF
 {
@@ -15,15 +17,41 @@ namespace FastHotKeyForWPF
     {
         private Key CurrentKey;
 
+        private KeySelectBox? LinkKey;
+
+        private bool NewStyle = false;
+
+        public KeyTypes KeyType
+        {
+            get
+            {
+                if (Enum.IsDefined(typeof(NormalKeys), CurrentKey.ToString()))
+                {
+                    return KeyTypes.Normal;
+                }
+                if (Enum.IsDefined(typeof(ModelKeys), CurrentKey.ToString()))
+                {
+                    return KeyTypes.Model;
+                }
+                return KeyTypes.None;
+            }
+        }
+
         public KeySelectBox()
         {
             if (PrefabComponent.TempInfo == null) { return; }
+            Width = 100;
+            Height = 50;
+            VerticalContentAlignment = VerticalAlignment.Center;
+            HorizontalContentAlignment = HorizontalAlignment.Center;
             IsReadOnly = true;
             FontSize = PrefabComponent.TempInfo.FontSize;
             Foreground = PrefabComponent.TempInfo.Foreground;
             Background = PrefabComponent.TempInfo.Background;
             Margin = PrefabComponent.TempInfo.Margin;
             KeyUp += WhileKeyUp;
+            MouseEnter += WhileMouseEnter;
+            MouseLeave += WhileMouseLeave;
         }
 
         public T? GetKey<T>() where T : class
@@ -55,6 +83,52 @@ namespace FastHotKeyForWPF
             Text = key.ToString();
             if (GlobalHotKey.IsDeBug) { MessageBox.Show($"已更新为【{key}】"); }
             e.Handled = true;
+        }
+
+        private void WhileMouseEnter(object sender, MouseEventArgs e)
+        {
+            Focus();
+            if (!NewStyle)
+            {
+                Background = Brushes.Black;
+                Foreground = Brushes.Cyan;
+            }
+        }
+
+        private void WhileMouseLeave(object sender, MouseEventArgs e)
+        {
+            Keyboard.ClearFocus();
+            if (!NewStyle)
+            {
+                Background = Brushes.Wheat;
+                Foreground = Brushes.Black;
+            }
+        }
+
+        public void UseFatherSize<T>() where T : UIElement
+        {
+            T? father = Parent as T;
+            if (father == null) { return; }
+
+            PropertyInfo? widthProperty = typeof(T).GetProperty("Width");
+            if (widthProperty == null) { return; }
+            object? width = widthProperty.GetValue(father);
+            if (width == null) { return; }
+            Width = (double)width;
+
+            PropertyInfo? heightProperty = typeof(T).GetProperty("Height");
+            if (heightProperty == null) { return; }
+            object? height = heightProperty.GetValue(father);
+            if (height == null) { return; }
+            Height = (double)height;
+        }
+
+        public void UseStyle(string stylename)
+        {
+            Style? style = (Style)FindResource(stylename);
+            if (style == null) return;
+            Style = style;
+            NewStyle = true;
         }
     }
 }
