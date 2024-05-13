d77cb4fc3d71d99405c4cc7c5c7b616b95446040
冯 <2789083329@qq.com>
2024-05-13 16:32:46 +0800

    完善注释

--- Component.cs
+++ Component.cs
@@ -8,6 +8,8 @@ namespace FastHotKeyForWPF
 {
     public interface Component
     {
-
+        //预制组件
+        //PrefabComponent.GetComponent<T>的泛型约束指向的就是此接口下的类型
+        //你也可以自定义一个UIElement并实现这个接口
     }
 }
--- ComponentInfo.cs
+++ ComponentInfo.cs
@@ -6,10 +6,17 @@ using System.Windows.Media;
 
 namespace FastHotKeyForWPF
 {
+    /// <summary>
+    /// 一个预制组件的基本信息
+    /// </summary>
     public class ComponentInfo
     {
         public ComponentInfo() { }
 
+        /// <param name="fontsize">字体大小</param>
+        /// <param name="foreground">字体颜色</param>
+        /// <param name="background">控件背景色</param>
+        /// <param name="margin">Margin属性</param>
         public ComponentInfo(double fontsize, SolidColorBrush foreground, SolidColorBrush background, Thickness margin)
         {
             FontSize = fontsize;
@@ -25,7 +32,7 @@ namespace FastHotKeyForWPF
         private double _margindown = 0;
         private double _marginleft = 0;
 
-        public double FontSize
+        public double FontSize//字体大小
         {
             get { return _fontsize; }
             set
@@ -37,11 +44,11 @@ namespace FastHotKeyForWPF
             }
         }
 
-        public SolidColorBrush Foreground = Brushes.Black;
+        public SolidColorBrush Foreground = Brushes.Black;//字体颜色
 
-        public SolidColorBrush Background = Brushes.White;
+        public SolidColorBrush Background = Brushes.White;//背景色
 
-        public Thickness Margin
+        public Thickness Margin//相对位置
         {
             get { return new Thickness(_marginup, _marginright, _margindown, _marginleft); }
             set
