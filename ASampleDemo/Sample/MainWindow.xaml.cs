b484b440d0c0c1b0da73f074b264120c4e4d3ce9
冯 <2789083329@qq.com>
2024-07-25 23:25:29 +0800

    提交示例Demo

---
+++ ASampleDemo/Sample/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FastHotKeyForWPF;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            GlobalHotKey.Awake();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            GlobalHotKey.Destroy();
+
+            base.OnClosed(e);
+        }
+
+        private void HandlerA(object sender, HotKeyEventArgs e)
+        {
+            int ID = e.RegisterInfo.RegisterID;
+
+            MessageBox.Show($"HotKeyA Has Been Invoked Whose ID Is {ID}");
+        }
+
+        private void HandlerB(object sender, HotKeyEventArgs e)
+        {
+            int ID = e.RegisterInfo.RegisterID;
+
+            MessageBox.Show($"HotKeyB Has Been Invoked Whose ID Is {ID}");
+        }
+    }
+}
---
+++ ASampleDemo/Sample/ViewModel/MyHotKeyBoxViewModelA.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using FastHotKeyForWPF;
+
+namespace Sample
+{
+    /// <summary>
+    /// 基于抽象基类设计 ViewModel 层 , 要求使用统一的 HotKeyBoxModel 作为 Model ( 已在基类中定义_model )
+    /// </summary>
+    public class MyHotKeyBoxViewModelA : HotKeyViewModelBase
+    {
+        public MyHotKeyBoxViewModelA()
+        {
+            _model = new HotKeyBoxModel();
+        }
+
+        private SolidColorBrush _fixedtransparent = Brushes.Transparent;
+        public SolidColorBrush FixedTransparent
+        {
+            get => _fixedtransparent;
+            set
+            {
+                if (_fixedtransparent != value)
+                {
+                    OnPropertyChanged(nameof(FixedTransparent));
+                }
+            }
+        }
+        //…
+        //拓展属性,它们不存在于Model,只负责逻辑数据的处理
+        //例如,将【UserControl的Background】与【FixedTransparent】做【TwoWay绑定】,即可永远保持为透明
+
+
+        //…
+        //重写属性,多数情况并不需要这一步
+
+
+        public override void UpdateText()
+        {
+            string? PossibilityA = (CurrentKeyA == Key.LeftCtrl || CurrentKeyA == Key.RightCtrl) ? "CTRL" : null;
+            string? PossibilityB = (CurrentKeyA == Key.LeftAlt || CurrentKeyA == Key.RightAlt) ? "ALT" : null;
+
+            string Left = (PossibilityA == null ? string.Empty : PossibilityA) + (PossibilityB == null ? string.Empty : PossibilityB);
+
+            Text = Left + " + " + CurrentKeyB.ToString();
+        }
+        //…
+        //重写方法
+        //例如,您希望不再区分 CTRL/ALT的左右,那么您可以重写UpdateText()
+    }
+}
