2d738f6419d2f46adf72d36a8215a323b7f23fb3
冯 <2789083329@qq.com>
2024-07-26 09:46:05 +0800

    尝试提交示例Demo

---
+++ ASampleDemo/Example/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FastHotKeyForWPF;
+
+namespace Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandlerA(object sender, HotKeyEventArgs e)
+        {
+            int ID = e.RegisterInfo.RegisterID;
+            //此处可获取热键的具体信息
+
+            MessageBox.Show($"HotKeyA Has Been Invoked Whose ID is {ID}");
+        }
+
+        private void HandlerB(object sender, HotKeyEventArgs e)
+        {
+            int ID = e.RegisterInfo.RegisterID;
+
+            MessageBox.Show($"HotKeyB Has Been Invoked Whose ID is {ID}");
+        }
+    }
+}
